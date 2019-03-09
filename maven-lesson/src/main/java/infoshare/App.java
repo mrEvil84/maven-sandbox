d0dc47d75585ce7aff96a6106516d1a6a7c7575e
pkowerzanow <piotr.kowerzanow@gmail.com>
2019-03-09 13:27:24 +0100

    initial commit

---
+++ maven-lesson/src/main/java/infoshare/App.java
@@ -0,0 +1,21 @@
+package infoshare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+
+
+
+        System.out.println( "Hello World!"
+
+
+
+        );
+    }
+}
---
+++ mavensandbox-test/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+P
+ src/main/java/infoshare/App.java,f/c/fc7a2b174ff877bdb127e1f29f945f1b9b7b870c
